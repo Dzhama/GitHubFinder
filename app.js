3ab1a03f884341d4caaa45375cc2182225ce9656
Dzhama <j.shoismatuloev@gmail.com>
2018-05-17 15:46:47 -0400

    Get profile is set, and Alert and Clear profile is added

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
+//init GitHub
+const github = new Github;
+//init ui
+
+const ui = new UI;
 // Search input
 const searchUser = document.querySelector('#searchUser');
 
 // Search input event Listener
-searchUser.addEventListener('submit', (e) => {
+searchUser.addEventListener('keyup', (e) => {
     //Get input text 
     const userText = e.target.value;
 
     if(userText !== "") {
-        console.log(userText);
+        // make http call
+        github.getUser(userText)
+        .then(data => {
+            if(data.profile.message === "Not Found"){
+                // show alert
+                ui.showAlert('User not found', 'alert alert-danger');
+                ui.clearProfile();
+            }else{
+                //show profile
+                ui.showProfile(data.profile);
+                console.log(data.profile)
+            }
+        })
+    }else {
+        // clear profile
     }
 })
--- github.js
+++ github.js
@@ -1,10 +1,16 @@
-class GitHub {
+class Github {
     constructor() {
-        this.client_id = "";
-        this.client_secret = "";
+        this.client_id = "81426aed6084af7ab428";
+        this.client_secret = "74beff13ee330c7ee806717c184b0e2de367201d";
     }
 
     async getUser(user) {
-        const profileResponse = await fetch (`https://api.github.com/users/${user}`)
+        const profileResponse = await fetch (`https://api.github.com/users/${user}?client_id${this.client_id}&client_secret${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+
+        return {
+            profile
+        }
     }
 }
